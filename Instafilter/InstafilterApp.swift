1bf1f6f06d081ee67108fca4a160cd5330edb70e
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-07-15 20:45:11 -0400

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by AlecNipp on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
