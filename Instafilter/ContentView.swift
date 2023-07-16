a230d864d2be92469ecbafd1d89b511155376da6
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-07-15 21:00:23 -0400

    sepia filter

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .onAppear(perform: loadImage)
+    }
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
+        
     }
 }
 
